7f9d1cc34a2f0208a21a53456e047bbee398ab65
Corentin <c.vaillant848@gmail.com>
2025-01-20 14:00:13 +0100

    advance on the parser, added face parser

--- src/mesh/obj_parser.rs
+++ src/mesh/obj_parser.rs
@@ -7,6 +7,14 @@ use crate::utils::macro_util::debug_println;
 
 use super::mesh::Mesh;
 
+struct FaceDataTripelet{
+    geo_vert_index      : u32,
+    texture_vertex_index: Option<u32>,
+    vertex_normal_index : u32,
+}
+
+type FaceData = Vec<FaceDataTripelet>;
+
 impl Mesh {
     pub fn load_from_wavefront<P: AsRef<Path>>(path: P) -> Result<Self, WavefrontError> {
         
@@ -15,6 +23,8 @@ impl Mesh {
         let mut texture_vert_data = vec![];
         let mut vertex_normal_data = vec![];
         let mut parameter_space_vertex = vec![];
+        let mut faces_data = vec![];
+        
 
         for line in read_to_string(path)
             .map_err(|_| WavefrontError::CouldNotReadFile)?
@@ -27,7 +37,7 @@ impl Mesh {
                 WaveFrontLineType::ParameterSpaceVert   => parameter_space_vertex.push(parse_tree_float(line,1.)),
                 WaveFrontLineType::Point                => debug_println!("Point not implemented yet"),
                 WaveFrontLineType::Line                 => debug_println!("Line not implemented yet"),
-                WaveFrontLineType::Face => todo!(),
+                WaveFrontLineType::Face                 => faces_data.push(parse_face(line)?),
                 WaveFrontLineType::Comment              => debug_println!("Fond comment :{}",line),
                 WaveFrontLineType::Empty                => (),    
                 WaveFrontLineType::Unknow               => debug_println!("Unknow or not yet implemented line :{}",line),
@@ -42,7 +52,10 @@ impl Mesh {
 #[derive(Debug)]
 pub enum WavefrontError {
     CouldNotReadFile,
+    InvalidFaceData,
 }
+
+
 
 fn line_type(line: &str) -> WaveFrontLineType {
     let mut split = line.split(" ");
@@ -103,4 +116,43 @@ fn parse_tree_float(line: &str,default:f32) -> [f32; 3] {
     }
 
     result
+}
+
+use core::str::FromStr;
+
+fn parse_face(line:&str)->Result<FaceData,WavefrontError>{
+    let split: Vec<&str> = line.split(' ').collect();
+
+    if split.get(0).is_none_or(|c|{*c!="f"}){
+        debug_println!("face line \"{}\" is invalide",line);
+        return Err(WavefrontError::InvalidFaceData);
+    }
+    let mut face_data = Vec::new();
+
+    for str_triplet in &split[1..] {
+        let mut indices = str_triplet.split('/');
+        let geo_vert_index = indices.next()
+            .and_then(|v| u32::from_str(v).ok())
+            .ok_or(WavefrontError::InvalidFaceData)?;
+
+        let texture_vertex_index = if let Some(str) = indices.next(){
+            str.parse().ok()
+        }else{
+            None
+        };
+
+
+        let vertex_normal_index = indices.next()
+            .and_then(|v| u32::from_str(v).ok())
+            .ok_or(WavefrontError::InvalidFaceData)?;
+
+
+        face_data.push(FaceDataTripelet {
+            geo_vert_index,
+            texture_vertex_index,
+            vertex_normal_index,
+        });
+    }
+
+    Ok(face_data)
 }

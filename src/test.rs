090b7135dd28bdc00e11d125d8a7d58b1f02f939
Corentin <c.vaillant848@gmail.com>
2025-01-22 12:26:36 +0100

    more test

--- src/test.rs
+++ src/test.rs
@@ -2,13 +2,13 @@
 mod test{
     use crate::mesh::mesh::Mesh;
     use crate::mesh::obj_parser::WaveFrontParsable;
+    use crate::object_traits::SceneObject;
 
     macro_rules! chrono {
         ($func:expr,$($arg:tt)*) => {{
             let now = std::time::Instant::now();
             let result = $func($($arg)*);
             let elapsed_time = now.elapsed();
-            print!("{} ",$($arg)*);
             println!("test OK ✅, took {}", elapsed_time.as_secs_f64());
             result}
         };
@@ -24,12 +24,15 @@ mod test{
     pub(super) fn mesh_test() {
         println!("->\ttest mesh");
         let triangle = test_valid_wavefront!("triangle");
-        let _cube     = test_valid_wavefront!("cube");
+        let mut cube     = test_valid_wavefront!("cube");
         let _sphere   = test_valid_wavefront!("sphere");
         let _suzanne  = test_valid_wavefront!("suzanne");
         let _teapot   = test_valid_wavefront!("teapot");
         let _bunny    = test_valid_wavefront!("bunny");
 
         assert_eq!(triangle.vertecies_number(),3);
+
+        chrono!(Mesh::translate,&mut cube,[1.,1.,1.].into());
+        chrono!(Mesh::apply_position,&mut cube)
     }
 }
